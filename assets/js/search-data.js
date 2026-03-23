035e0901b63392e61e5bdf1c05bb6df7e921ef92
kanamoroo <141616544+kanamoroo@users.noreply.github.com>
2026-03-23 08:00:29 +0000

    Deploying to gh-pages from @ kanamoroo/academic-website@a3e9c97e7ab931ee1166455760b256d9929bc491 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -331,17 +331,6 @@ ninja.data = [{
             window.location.href = "/academic-website/blog/2020/twitter/";
           
         },
-      },{id: "post-a-post-with-disqus-comments",
-        
-          title: "a post with disqus comments",
-        
-        description: "an example of a blog post with disqus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/academic-website/blog/2015/disqus-comments/";
-          
-        },
       },{id: "post-a-post-with-math",
         
           title: "a post with math",
