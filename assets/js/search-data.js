7a615c55dc9e781b11f6a0738555fbc1e1800fc1
kanamoroo <141616544+kanamoroo@users.noreply.github.com>
2026-03-23 08:33:02 +0000

    Deploying to gh-pages from @ kanamoroo/academic-website@857b225c6cdd674645dbeb8e11308f8dce03ab12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -61,11 +61,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/academic-website/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/academic-website/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
